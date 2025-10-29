8103810babcfc281970c57ef2de8157f83e85b84
Harsh Ganjsinghani <aganjsinghani@gmail.com>
2025-10-29 12:11:38 +0530

    project_setup

---
+++ eslint.config.mjs
@@ -0,0 +1,28 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+import tseslint from "typescript-eslint";
+import js from "@eslint/js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.config({
+    extends: [
+      "next/core-web-vitals",
+      "next/typescript", "prettier",
+    ],
+    plugins: ["prettier"],
+    rules: {
+      "prettier/prettier": "error",
+      "react/no-escape-entities": "off",
+    },
+  }),
+];
+
+export default eslintConfig;
