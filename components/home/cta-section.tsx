c5c0e18062cd29ced04203c2adc975f3281495e0
simrangajsinghani <simrangajsinghani259@gmail.com>
2025-11-09 02:38:30 +0800

    FE changes

---
+++ components/home/cta-section.tsx
@@ -0,0 +1,39 @@
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { ArrowRight } from "lucide-react";
+
+export default function CTASection() {
+  return (
+    <section className="bg-gray-50 py-12">
+      <div className="py-12 lg:py-24 max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 lg:pt-12">
+        <div className="flex flex-col items-center justify-center text-center space-y-4">
+          <div className="space-y-2">
+            <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">
+              Ready to Save Hours of Reading Time?
+            </h2>
+            <p className="mx-auto max-w-2xl text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
+              Transform lengthy documents into clear, actionable insights with
+              AI-powered summarizer
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-col gap-2 sm:flex-row justify-center">
+          <div>
+            <Button
+              variant={"link"}
+              className="w-full min-[400px]:w-auto bg-gradient-to-r from-slate-900 to-rose-500 hover:from-rose-500 hover:to-slate-900 hover:text-white text-white transition-all duration-300 mt-4"
+            >
+              <Link
+                href="/#pricing"
+                className="flex items-center justify-center"
+              >
+                Get Started
+                <ArrowRight className="ml-2 h-4 w-4 animate-pulse" />
+              </Link>
+            </Button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
